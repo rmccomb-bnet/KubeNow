b9697514485a333e8b89b159788a64f98eb70ad3
Anders Larsson <anders.larsson@icm.uu.se>
2017-12-13 12:07:00 +0100

    updates [skip ci]

--- common/inventory/main.tf
+++ common/inventory/main.tf
@@ -5,52 +5,59 @@ variable domain {}
 variable master_as_edge {}
 
 variable master_hostnames {
-  type = "list"
+  type    = "list"
+  default = [""]
 }
 
 variable master_public_ip {
-  type = "list"
+  type    = "list"
+  default = [""]
 }
 
 variable master_private_ip {
-  type = "list"
-}
-
-variable edge_count {}
-
-variable edge_hostnames {
-  type = "list"
-}
-
-variable edge_public_ip {
-  type = "list"
-}
-
-variable edge_private_ip {
-  type = "list"
+  type    = "list"
+  default = [""]
 }
 
 variable node_count {}
 
 variable node_hostnames {
-  type = "list"
+  type    = "list"
+  default = [""]
 }
 
 variable node_public_ip {
-  type = "list"
+  type    = "list"
+  default = [""]
 }
 
 variable node_private_ip {
-  type = "list"
+  type    = "list"
+  default = [""]
+}
+
+variable edge_count {}
+
+variable edge_hostnames {
+  type    = "list"
+  default = [""]
+}
+
+variable edge_public_ip {
+  type    = "list"
+  default = [""]
+}
+
+variable edge_private_ip {
+  type    = "list"
+  default = [""]
 }
 
 variable glusternode_count {}
 variable gluster_volumetype {}
 variable extra_disk_device {}
 
-variable inventory_template_file {
-  default = "inventory-template"
-}
+variable inventory_template {}
 
 variable inventory_output_file {
   default = "inventory"
@@ -58,6 +65,20 @@ variable inventory_output_file {
 
 # create variables
 locals {
+  master_hostnames  = "${split(",", length(var.master_hostnames) == 0 ? join(",", list("")) : join(",", var.master_hostnames))}"
+  master_public_ip  = "${split(",", length(var.master_public_ip) == 0 ? join(",", list("")) : join(",", var.master_public_ip))}"
+  master_private_ip = "${split(",", length(var.master_private_ip) == 0 ? join(",", list("")) : join(",", var.master_private_ip))}"
+
+  node_hostnames  = "${split(",", length(var.node_hostnames) == 0 ? join(",", list("")) : join(",", var.node_hostnames))}"
+  node_public_ip  = "${split(",", length(var.node_public_ip) == 0 ? join(",", list("")) : join(",", var.node_public_ip))}"
+  node_private_ip = "${split(",", length(var.node_private_ip) == 0 ? join(",", list("")) : join(",", var.node_private_ip))}"
+
+  # Format list of different node types
+  masters = "${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=%s", local.master_hostnames , local.master_public_ip, var.ssh_user ))}"
+
+  nodes = "${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=%s", local.node_hostnames , local.node_public_ip, var.ssh_user))}"
+
+
   # Format list of masters
   masters = "${join("\n",formatlist("%s ansible_ssh_host=%s ansible_ssh_user=%s", var.master_hostnames , var.master_public_ip, var.ssh_user))}"
   
@@ -76,8 +97,8 @@ locals {
 
 # Generate inventory from template file
 data "template_file" "inventory" {
-  template = "${file("${path.root}/../${ var.inventory_template_file }")}"
-  
+  template = "${file("${path.root}/../${ var.inventory_template }")}"
+
   vars {
     masters            = "${local.masters}"
     nodes              = "${local.nodes}"
@@ -102,6 +123,6 @@ resource "null_resource" "local" {
   }
 
   provisioner "local-exec" {
-    command = "echo '${data.template_file.inventory.rendered}' > \"${path.root}/../${var.inventory_output_file}\""
+    command = "echo \"${data.template_file.inventory.rendered}\" > \"${path.root}/../${var.inventory_output_file}\""
   }
 }
